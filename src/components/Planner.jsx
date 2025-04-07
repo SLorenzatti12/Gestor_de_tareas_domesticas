af05f39de310152c124bb5f40075ec6631f34344
Santiago Lorenzatti <slorenzatti@facultad.sanfrancisco.utn.edu.ar>
2025-04-06 23:30:55 -0300

    Creacion de componentes y hoja de estilos

---
+++ src/components/Planner.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import EventForm from "./EventForm";
+import EventList from "./EventList";
+import "./styles.css";
+
+const Planner = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const storedEvents = JSON.parse(localStorage.getItem("events")) || [];
+    setEvents(storedEvents);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("events", JSON.stringify(events));
+  }, [events]);
+
+  const addEvent = (newEvent) => {
+    setEvents([...events, newEvent]);
+  };
+
+  const markAsCompleted = (id) => {
+    setEvents(events.map(event =>
+      event.id === id ? { ...event, status: "Completado" } : event
+    ));
+  };
+
+  return (
+    <div className="planner">
+      <h1>Planificador de Rutinas</h1>
+      <EventForm addEvent={addEvent} />
+      <EventList events={events} markAsCompleted={markAsCompleted} />
+    </div>
+  );
+};
+
+export default Planner;
